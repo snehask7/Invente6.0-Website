f7a4d18e48e36518728df23c278879385238b72e
Harisaran <hari@luxecraft.org>
2021-08-21 18:49:41 +0530

    fix: lint error

--- next.config.js
+++ next.config.js
@@ -1,17 +1,17 @@
 module.exports = {
   reactStrictMode: true,
-}
+};
 
-const path = require('path')
+const path = require('path');
 const withSass = require('@zeit/next-sass');
 module.exports = withSass({
-/* bydefault config  option Read For More Optios
+  /* bydefault config  option Read For More Optios
 here https://github.com/vercel/next-plugins/tree/master/packages/next-sass*/
-cssModules: true
-})
+  cssModules: true,
+});
 module.exports = {
-/* Add Your Scss File Folder Path Here */
-sassOptions: {
-includePaths: [path.join(__dirname, 'styles')],
-},
-}
+  /* Add Your Scss File Folder Path Here */
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+};
--- pages/signin.js
+++ pages/signin.js
@@ -1,50 +1,85 @@
 import Link from 'next/link';
-import React, { useState } from "react";
-import Typewriter from "typewriter-effect";
-import styles2 from "../styles/Auth.module.css";
-import styles from "../styles/Signin.module.css";
-function Signin() {
+import React, { useState } from 'react';
+import Typewriter from 'typewriter-effect';
+import styles2 from '../styles/Auth.module.css';
+import styles from '../styles/Signin.module.css';
 
-  const [formEmail, setFormEmail] = useState("");
-  const [formPassword, setFormPassword] = useState("");
+function Signin() {
+  const [formEmail, setFormEmail] = useState('');
+  const [formPassword, setFormPassword] = useState('');
 
   const onSubmit = () => {
-    event.preventDefault()
-  }
+    event.preventDefault();
+  };
 
-  return <div className={styles.container}>
-
-    <div className={styles2.animation_wrapper}>
-      <div className={styles2["particle"] + " " + styles2["particle_1"]} ></div>
-      <div className={styles2["particle"] + " " + styles2["particle_2"]}></div>
-      <div className={styles2["particle"] + " " + styles2["particle_3"]}></div>
-      <div className={styles2["particle"] + " " + styles2["particle_4"]}></div>
-    </div>
-
-    <main className={styles2.page_wrapper}>
-      <div className={styles.signinCard}>
-        <h1 className={styles.typing}><Typewriter
-          onInit={(typewriter) => {
-            typewriter.changeDelay(100)
-              .typeString('Welcome back!')
-              .start()
-              .callFunction(() => {
-                typewriter.stop().pauseFor(200)
-              })
-          }}
-        /></h1>
-        <form className="form" onSubmit={onSubmit}>
-          <input type="email" id="form-email" placeholder="Email" value={formEmail} onChange={(e) => setFormEmail(e.target.value)} className={styles.input} />
-
-          <input type="password" id="form-password" placeholder="Password" value={formPassword} onChange={(e) => setFormPassword(e.target.value)} className={styles.input} />
-          <Link href=""><a className={styles.text_muted}> Forgot password?</a></Link>
-          <button type="submit" className={styles.btn_signin}>Sign In</button>
-        </form>
-        <h6 className={styles.signUp}>Don`t have an account?<span style={{ color: '#0ff' }}> <Link href="/signup">Sign Up</Link></span></h6>
+  return (
+    <div className={styles.container}>
+      <div className={styles2.animation_wrapper}>
+        <div
+          className={styles2['particle'] + ' ' + styles2['particle_1']}
+        ></div>
+        <div
+          className={styles2['particle'] + ' ' + styles2['particle_2']}
+        ></div>
+        <div
+          className={styles2['particle'] + ' ' + styles2['particle_3']}
+        ></div>
+        <div
+          className={styles2['particle'] + ' ' + styles2['particle_4']}
+        ></div>
       </div>
-    </main>
-  </div>
-    ;
-};
+
+      <main className={styles2.page_wrapper}>
+        <div className={styles.signinCard}>
+          <h1 className={styles.typing}>
+            <Typewriter
+              onInit={(typewriter) => {
+                typewriter
+                  .changeDelay(100)
+                  .typeString('Welcome back!')
+                  .start()
+                  .callFunction(() => {
+                    typewriter.stop().pauseFor(200);
+                  });
+              }}
+            />
+          </h1>
+          <form className="form" onSubmit={onSubmit}>
+            <input
+              type="email"
+              id="form-email"
+              placeholder="Email"
+              value={formEmail}
+              onChange={(e) => setFormEmail(e.target.value)}
+              className={styles.input}
+            />
+
+            <input
+              type="password"
+              id="form-password"
+              placeholder="Password"
+              value={formPassword}
+              onChange={(e) => setFormPassword(e.target.value)}
+              className={styles.input}
+            />
+            <Link href="">
+              <a className={styles.text_muted}> Forgot password?</a>
+            </Link>
+            <button type="submit" className={styles.btn_signin}>
+              Sign In
+            </button>
+          </form>
+          <h6 className={styles.signUp}>
+            Don`t have an account?
+            <span style={{ color: '#0ff' }}>
+              {' '}
+              <Link href="/signup">Sign Up</Link>
+            </span>
+          </h6>
+        </div>
+      </main>
+    </div>
+  );
+}
 
 export default Signin;
