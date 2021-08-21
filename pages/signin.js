ec2a00837b563bc61c6ebea5d1ebd51a6ddfce7f
Sneha Sriram Kannan <52703275+snehask7@users.noreply.github.com>
2021-08-21 15:56:24 +0530

    Scss to css
    Converted scss to css for particles background, removed node-sass

--- pages/signin.js
+++ pages/signin.js
@@ -1,50 +1,50 @@
-import React, {useState} from "react";
+import Link from 'next/link';
+import React, { useState } from "react";
 import Typewriter from "typewriter-effect";
-import styles from "../styles/Signin.module.css"
-import styles2 from "../styles/Auth.module.scss"
-import Link from 'next/link'
+import styles2 from "../styles/Auth.module.css";
+import styles from "../styles/Signin.module.css";
 function Signin() {
 
-    const [formEmail, setFormEmail] = useState("");
-    const [formPassword, setFormPassword] = useState("");
+  const [formEmail, setFormEmail] = useState("");
+  const [formPassword, setFormPassword] = useState("");
 
-    const onSubmit = () => {
-        event.preventDefault()
-    }
+  const onSubmit = () => {
+    event.preventDefault()
+  }
 
-    return <div className={styles.container}>
-            
-        <div className={styles2.animation_wrapper}>
-        <div className={styles2["particle"] + " " + styles2["particle_1"]} ></div>
-        <div className={styles2["particle"] + " " + styles2["particle_2"]}></div>
-        <div className={styles2["particle"] + " " + styles2["particle_3"]}></div>
-        <div className={styles2["particle"] + " " + styles2["particle_4"]}></div>
-        </div>
+  return <div className={styles.container}>
 
-            <main className={styles2.page_wrapper}>
-                <div className={styles.signinCard}>
-                <h1 className={styles.typing}><Typewriter
-                onInit={(typewriter)=>{
-                    typewriter.changeDelay(100)
-                    .typeString('Welcome back!')
-                    .start()
-                    .callFunction(()=>{
-                        typewriter.stop().pauseFor(200)
-                    })
-                }}
-                /></h1>
-                 <form className="form" onSubmit={onSubmit}>
-                    <input type="email" id="form-email" placeholder="Email" value={formEmail} onChange={(e) => setFormEmail(e.target.value)} className={styles.input} />
+    <div className={styles2.animation_wrapper}>
+      <div className={styles2["particle"] + " " + styles2["particle_1"]} ></div>
+      <div className={styles2["particle"] + " " + styles2["particle_2"]}></div>
+      <div className={styles2["particle"] + " " + styles2["particle_3"]}></div>
+      <div className={styles2["particle"] + " " + styles2["particle_4"]}></div>
+    </div>
 
-                    <input type="password" id="form-password" placeholder="Password" value={formPassword} onChange={(e) => setFormPassword(e.target.value)}  className={styles.input}/>
-                    <Link href=""><a className={styles.text_muted}> Forgot password?</a></Link>
-                    <button type="submit" className={styles.btn_signin}>Sign In</button>
-                    </form>
-                    <h6 className={styles.signUp}>Don`t have an account?<span style={{ color: '#0ff' }}> <Link href="/signup">Sign Up</Link></span></h6>
-                </div>
-            </main>
-            </div>
-;
+    <main className={styles2.page_wrapper}>
+      <div className={styles.signinCard}>
+        <h1 className={styles.typing}><Typewriter
+          onInit={(typewriter) => {
+            typewriter.changeDelay(100)
+              .typeString('Welcome back!')
+              .start()
+              .callFunction(() => {
+                typewriter.stop().pauseFor(200)
+              })
+          }}
+        /></h1>
+        <form className="form" onSubmit={onSubmit}>
+          <input type="email" id="form-email" placeholder="Email" value={formEmail} onChange={(e) => setFormEmail(e.target.value)} className={styles.input} />
+
+          <input type="password" id="form-password" placeholder="Password" value={formPassword} onChange={(e) => setFormPassword(e.target.value)} className={styles.input} />
+          <Link href=""><a className={styles.text_muted}> Forgot password?</a></Link>
+          <button type="submit" className={styles.btn_signin}>Sign In</button>
+        </form>
+        <h6 className={styles.signUp}>Don`t have an account?<span style={{ color: '#0ff' }}> <Link href="/signup">Sign Up</Link></span></h6>
+      </div>
+    </main>
+  </div>
+    ;
 };
 
 export default Signin;
