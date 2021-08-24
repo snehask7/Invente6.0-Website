62391b5fad9a6651621c3286a74a002ba5480d18
Sneha Sriram Kannan <52703275+snehask7@users.noreply.github.com>
2021-08-24 15:40:39 +0530

    Added Event Cards

--- pages/departmentPage.js
+++ pages/departmentPage.js
@@ -1,9 +1,15 @@
 import { useRouter } from 'next/router';
 import React, { useState } from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
-import { FaCalendarAlt, FaPhoneAlt, FaRegBuilding, FaUserAlt } from 'react-icons/fa';
+import {
+  FaCalendarAlt,
+  FaPhoneAlt,
+  FaRegBuilding,
+  FaTrophy,
+  FaUserAlt
+} from 'react-icons/fa';
 import styles from '../styles/DepartmentPage.module.css';
-const departmentPage = () => {
+const DepartmentPage = () => {
   const [state, setState] = useState({});
   const router = useRouter();
 
@@ -12,63 +18,122 @@ const departmentPage = () => {
       <main>
         <h1 className={styles.pageHeading}>Department of CSE</h1>
         <Container>
-          <Row>
-            <Col lg={4} md={12} sm={12}>
-              <Row className={styles.eventsContainer}>
+          <Row className={styles.wrapper}>
+            <Col
+              lg={12}
+              xl={4}
+              md={12}
+              sm={12}
+              className={styles.eventsContainer}
+            >
+              <Row>
                 <Col>
                   <div className={styles.eventCard}>
+                    <span></span>
+                    <div className={styles.content}>
+                      <div className={styles.content}>
+                        <p>Devathlon</p>
+                      </div>
+                    </div>
+                  </div>
+                </Col>
+                <Col>
+                  <div className={styles.eventCard}>
+                    <span></span>
                     <p>Devathlon</p>
                   </div>
                 </Col>
                 <Col>
                   <div className={styles.eventCard}>
+                    <span></span>
                     <p>Devathlon</p>
                   </div>
                 </Col>
                 <Col>
                   <div className={styles.eventCard}>
+                    <span></span>
                     <p>Devathlon</p>
                   </div>
                 </Col>
                 <Col>
                   <div className={styles.eventCard}>
+                    <span></span>
                     <p>Devathlon</p>
                   </div>
                 </Col>
                 <Col>
                   <div className={styles.eventCard}>
+                    <span></span>
                     <p>Devathlon</p>
                   </div>
                 </Col>
                 <Col>
                   <div className={styles.eventCard}>
+                    <span></span>
                     <p>Devathlon</p>
                   </div>
                 </Col>
                 <Col>
                   <div className={styles.eventCard}>
-                    <p>Devathlon</p>
-                  </div>
-                </Col>
-                <Col>
-                  <div className={styles.eventCard}>
+                    <span></span>
                     <p>Devathlon</p>
                   </div>
                 </Col>
               </Row>
             </Col>
-            <Col lg={8} md={12} sm={12}>
+            <Col lg={12} xl={8} md={12} sm={12}>
               <div className={styles.eventDetails}>
-                <h1 className={styles.eventHeading}>App-Off</h1><br></br>
-                <p className={styles.eventDesc}>A coding competition based purely on Data structures and Algorithms.Participants will contest against each other in a competitive programming style contest.</p>
+                <h1 className={styles.eventHeading}>App-Off</h1>
+                <br></br>
+                <p className={styles.eventDesc}>
+                  A coding competition based purely on Data structures and
+                  Algorithms.Participants will contest against each other in a
+                  competitive programming style contest.
+                </p>
                 <hr></hr>
                 <Row>
                   <Col md={3}>
                     <Row className={styles.iconContainer}>
-                      <Col className={styles.iconStyling} sm={12} md={12} lg={12}><FaPhoneAlt></FaPhoneAlt>&nbsp;+91-4884898492</Col>
-                      <Col className={styles.iconStyling} sm={12} md={12} lg={12}><FaUserAlt></FaUserAlt>&nbsp;2 per team</Col>
-                      <Col className={styles.iconStyling} sm={12} md={12} lg={12}><FaCalendarAlt></FaCalendarAlt>&nbsp;Oct 7th, 9:00 AM</Col>
-                      <Col className={styles.iconStyling} sm={12} md={12} lg={12}><FaRegBuilding></FaRegBuilding>&nbsp;Any department</Col>
+                      <Col
+                        className={styles.iconStyling}
+                        sm={12}
+                        md={12}
+                        lg={12}
+                      >
+                        <FaPhoneAlt></FaPhoneAlt>&nbsp;+91-4884898492
+                      </Col>
+                      <Col
+                        className={styles.iconStyling}
+                        sm={12}
+                        md={12}
+                        lg={12}
+                      >
+                        <FaUserAlt></FaUserAlt>&nbsp;2 per team
+                      </Col>
+                      <Col
+                        className={styles.iconStyling}
+                        sm={12}
+                        md={12}
+                        lg={12}
+                      >
+                        <FaCalendarAlt></FaCalendarAlt>&nbsp;Oct 7th, 9:00 AM
+                      </Col>
+                      <Col
+                        className={styles.iconStyling}
+                        sm={12}
+                        md={12}
+                        lg={12}
+                      >
+                        <FaRegBuilding></FaRegBuilding>&nbsp;Any department
+                      </Col>
+                      <Col
+                        className={styles.iconStyling}
+                        sm={12}
+                        md={12}
+                        lg={12}
+                      >
+                        <FaTrophy></FaTrophy>&nbsp;Rs.5000
+                      </Col>
                     </Row>
                   </Col>
                   <Col md={4}>
@@ -88,6 +153,6 @@ const departmentPage = () => {
       </main>
     </div>
   );
-}
+};
 
-export default departmentPage;
+export default DepartmentPage;
