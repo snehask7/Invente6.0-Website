28d4c49ae4e0cfc66f7a8d28e013d622688ff5bc
Siddharth <siddharth18150@cse.ssn.edu.in>
2021-08-27 12:09:55 +0530

    removed try-catch in register.js, added res after update

--- pages/api/register.js
+++ pages/api/register.js
@@ -7,21 +7,21 @@ export default async function handler(req, res) {
 
   if (req.method === 'POST') {
     const { username, eventid } = req.body;
-
     // bad request format
-    if (username === null || eventid === null) {
+    if (username == null || eventid == null) {
       res.status(400).send({
         message: 'Bad request',
         error: 'One are more body parameters are missing',
       });
     } else {
       const userDoc = firestore.collection('users').doc(username);
-      let result;
-
       try {
-        result = await userDoc.update({
+        await userDoc.update({
           events: arrayUnion(eventid),
           updatedAt: serverTimestamp(),
+        });
+        res.status(200).send({
+          message: 'User registered for event successfully',
         });
       } catch (err) {
         //console.error(err);
@@ -29,20 +29,6 @@ export default async function handler(req, res) {
           message: 'Bad request',
           error: 'Username not found',
         });
-      }
-
-      // if result exists (document exists and has been updated)
-      if (result) {
-        try {
-          res.status(200).send({
-            message: 'User registered for event successfully',
-          });
-        } catch (err) {
-          res.status(500).send({
-            message: 'Server error',
-            error: err.toString(),
-          });
-        }
       }
     }
   }
