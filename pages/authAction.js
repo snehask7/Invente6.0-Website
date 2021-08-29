665473d78e9aadf36054a27e3ccfb5a9fba96c80
Sneha Sriram Kannan <52703275+snehask7@users.noreply.github.com>
2021-08-30 02:13:43 +0530

    eslint

--- pages/authAction.js
+++ pages/authAction.js
@@ -8,7 +8,7 @@ import styles2 from '../styles/Auth.module.css';
 import styles from '../styles/Signin.module.css';
 function Signin() {
   const router = useRouter();
-  const [email, setEmail] = useState()
+  const [email, setEmail] = useState();
   const { query } = useRouter();
   var mode = query.mode;
   var actionCode = query.oobCode;
@@ -20,7 +20,7 @@ function Signin() {
     })
     .catch(function () {
       // Invalid code
-      alert('Invalid page! Please use the link in your email')
+      alert('Invalid page! Please use the link in your email');
     });
   return (
     <div className={styles.container}>
@@ -35,48 +35,47 @@ function Signin() {
       <NavbarComp />
       <main className={styles2.page_wrapper}>
         <div className={styles.signinCard}>
-          {
-            mode == 'verifyEmail' ? (
-              <h1 className={styles.mailBoxText}>
-                <h4>Your email has been verified</h4>
-                <br />
-                You can now sign in with your new account!{' '}
-                <span style={{ color: 'rgba(0, 225, 255, 0.87)' }}>
-                  {' '}
-                  <Link href="/signin">Sign In</Link>
-                </span>
-              </h1>
-            ) : null
-              (
-                page == 'resetPassword' ?
-                  <>
-                    <h1 className={styles.mailBoxText}>
-                      <h4>Reset your password</h4>
-                      <br />
-                      for {email}<br />
-                      <Row>
-                        <input
-                          type="password"
-                          id="form-password"
-                          onChange={(e) => setFormPassword(e.target.value)}
-                          value={formPassword}
-                          className={styles.inputField}
-                          required
-                        />
-                        <button
-                          type="submit"
-                          className={styles.continueButton}
-                          onClick={() => onContinue()}
-                        >
-                          {buttonText}
-                        </button>
-                      </Row>
-                    </h1>
-
-                  </>
-                  : null
-              )
-          }
+          {mode == 'verifyEmail' ? (
+            <h1 className={styles.mailBoxText}>
+              <h4>Your email has been verified</h4>
+              <br />
+              You can now sign in with your new account!{' '}
+              <span style={{ color: 'rgba(0, 225, 255, 0.87)' }}>
+                {' '}
+                <Link href="/signin">Sign In</Link>
+              </span>
+            </h1>
+          ) : (
+            null(
+              page == 'resetPassword' ? (
+                <>
+                  <h1 className={styles.mailBoxText}>
+                    <h4>Reset your password</h4>
+                    <br />
+                    for {email}
+                    <br />
+                    <Row>
+                      <input
+                        type="password"
+                        id="form-password"
+                        onChange={(e) => setFormPassword(e.target.value)}
+                        value={formPassword}
+                        className={styles.inputField}
+                        required
+                      />
+                      <button
+                        type="submit"
+                        className={styles.continueButton}
+                        onClick={() => onContinue()}
+                      >
+                        {buttonText}
+                      </button>
+                    </Row>
+                  </h1>
+                </>
+              ) : null
+            )
+          )}
         </div>
       </main>
     </div>
