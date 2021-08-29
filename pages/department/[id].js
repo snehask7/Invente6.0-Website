eff4bd1df6a2ea99f25e7dcf8a5602c56ebcd75b
Sneha Sriram Kannan <52703275+snehask7@users.noreply.github.com>
2021-08-30 01:55:45 +0530

    forgotPassword

--- pages/department/[id].js
+++ pages/department/[id].js
@@ -219,6 +219,7 @@ export function getStaticPaths() {
       { params: { id: 'BME' } },
       { params: { id: 'EEE' } },
       { params: { id: 'IT' } },
+      { params: { id: 'CSE' } },
       { params: { id: 'Chemical' } },
     ],
     fallback: false,
