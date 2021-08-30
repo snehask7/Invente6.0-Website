2570f5935c3375e876a771b344afba67712e9148
Sneha Sriram Kannan <52703275+snehask7@users.noreply.github.com>
2021-08-30 09:29:27 +0530

    forgotPassword

--- pages/authAction.js
+++ pages/authAction.js
@@ -7,7 +7,6 @@ import { toast } from 'react-hot-toast';
 import NavbarComp from '../components/Navbar';
 import styles2 from '../styles/Auth.module.css';
 import styles from '../styles/Signin.module.css';
-
 function Signin() {
   const router = useRouter();
   const [email, setEmail] = useState();
@@ -21,16 +20,18 @@ function Signin() {
     .then(function (email) {
       setEmail(email);
     })
-    .catch(function () {
-      // Invalid code
+    .catch((error) => {
       toast.error('Invalid page! Please use the link in your email');
+      router.push('/signin');
     });
   function changePassword() {
-    const app = firebase.initializeApp(config);
-    const auth = getAuth(app);
-    confirmPasswordReset(auth, actionCode, password)
+    event.preventDefault();
+    firebase
+      .auth()
+      .confirmPasswordReset(actionCode, password)
       .then((resp) => {
         toast.success('Password Updated');
+        router.push('/signin');
       })
       .catch((error) => {
         toast.error('URL has expired or password too weak!');
@@ -50,32 +51,39 @@ function Signin() {
       <main className={styles2.page_wrapper}>
         <div className={styles.signinCard}>
           {mode == 'verifyEmail' ? (
-            <h1 className={styles.mailBoxText}>
-              <h4>Your email has been verified</h4>
+            <>
+              <h4 className={styles.mailBoxText}>
+                Your email has been verified
+              </h4>
               <br />
-              You can now sign in with your new account!{' '}
-              <span style={{ color: 'rgba(0, 225, 255, 0.87)' }}>
-                {' '}
-                <Link href="/signin">Sign In</Link>
-              </span>
-            </h1>
+              <h1 className={styles.mailBoxText}>
+                You can now sign in with your new account!{' '}
+                <span style={{ color: 'rgba(0, 225, 255, 0.87)' }}>
+                  {' '}
+                  <Link href="/signin">Sign In</Link>
+                </span>
+              </h1>
+            </>
           ) : mode == 'resetPassword' ? (
             <>
               <h1 className={styles.mailBoxText}>
                 <h4>Reset your password</h4>
-                <br />
                 for {email}
+                <br />
                 <br />
                 <Row>
                   <form onSubmit={changePassword}>
-                    <input
-                      type="password"
-                      onChange={(e) => setPassword(e.target.value)}
-                      value={password}
-                      className={styles.inputField}
-                      placeHolder="Enter new password"
-                      required
-                    />
+                    <h5 className={styles.inputLabel}>New Password</h5>
+                    <div className={styles.terminalInput}>
+                      <p>{'>'}</p>
+                      <input
+                        type="password"
+                        onChange={(e) => setPassword(e.target.value)}
+                        value={password}
+                        className={styles.inputField}
+                        required
+                      />
+                    </div>
                     <button type="submit" className={styles.continueButton}>
                       Save
                     </button>
