276e70576e54c7e0eecb9f03fea330c814b3c22b
Harisaran <hari@luxecraft.org>
2021-09-02 22:19:24 +0530

    add: `photoURL` in signup page

--- pages/signup.js
+++ pages/signup.js
@@ -28,7 +28,7 @@ function SignUp() {
     department: '',
     year: '',
   });
-  const { signup, currentUser, addDisplayName } = useAuth();
+  const { signup, currentUser, addUserDetails } = useAuth();
   const [loading, setLoading] = useState(false);
   const router = useRouter();
 
@@ -59,9 +59,13 @@ function SignUp() {
         state['password']
       );
       if (currentUser) {
+        const username =
+          state['full_name'].toLowerCase().replace(' ', '_') +
+          generate4DigitNumber();
+        const dicebearURL = `https://avatars.dicebear.com/api/bottts/${username}.svg`;
         await currentUser.sendEmailVerification();
         toast.success('Verification email sent');
-        await addDisplayName(currentUser, state['full_name']);
+        await addUserDetails(currentUser, state['full_name'], dicebearURL);
         axios({
           baseURL: window.location.origin,
           method: 'POST',
@@ -69,9 +73,7 @@ function SignUp() {
           data: {
             uid: currentUser.uid,
             email: state['email'],
-            username:
-              state['full_name'].toLowerCase().replace(' ', '_') +
-              generate4DigitNumber(),
+            username: username,
             fullName: state['full_name'],
             phone: state['phone_number'],
             registerNumber: state['reg_no'],
