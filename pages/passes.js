4a6083895803106e79434d7b4fb66f455905f111
Sneha Sriram Kannan <52703275+snehask7@users.noreply.github.com>
2021-09-05 17:08:52 +0530

    added event passes

--- pages/passes.js
+++ pages/passes.js
@@ -29,56 +29,195 @@ function Plans() {
             <div className={styles.feature}>
               <Row className={styles.pricing_plans}>
                 <Col className={styles.pricing_plan}>
-                  <div className={styles.pricing_plan_extras}>
-                    <ul>
-                      <span>Includes:</span>
-                      <li>Upto 1000 reviews /mo</li>
-                      <li>Advanced Analytics</li>
-                      <li>Patreon/Kofi/BuyMeACoffee Link</li>
-                      <li>Unlimited Websites</li>
-                      <li>Bio, Social Links</li>
-                      <li>List multiple products</li>
-
-                    </ul>
-                  </div>
-                </Col>
-              </Row>
-              <Row className={styles.pricing_plans}>
-                <Col className={styles.pricing_plan}>
-                  <div className={styles.plan_bg}><Image width="350" height="500" src="/Plans/purple.svg" alt="plan background" /></div>
+                  <div className={styles.plan_bg}><Image width="400" height="600" src="/Plans/purple.svg" alt="plan background" /></div>
                   <img src="https://wee.bio/assets/pattern/super-obj-1.png" alt="plan background" className={styles.obj_1} />
                   <div className={styles.super_tier}>
-                    <div className={styles.plan_title}>Super</div>
+                    <div className={styles.plan_title}>Combo</div>
                     <div className={styles.plan_price}>
-                      <div className={styles.dollar}>$</div>
-                      <div className={styles.price}>3</div>
-                      <div className={styles.type}>/ month</div>
+                      <div className={styles.dollar}>Rs.</div>
+                      <div className={styles.price}>300</div>
                     </div>
                     <div className={styles.plan_description}>
-                      <span>For Power Makers</span>
-                      <span> Growing their businesses and collecting a lot </span>
+                      <span>All Tech + All Non-Tech + AI/ML Workshop)</span>
                     </div>
                   </div>
                   <div className={styles.plan_action}>
-                    <a href="/register?plan=super" className={styles.btn + " " + styles.btn_big + " " + styles.btn_primary}>GET SUPER FOR $3</a>
+                    <a >Pay Now </a>
+                  </div>
+                  <div className={styles.plan_action}>
+                    <a >Upload Receipt</a>
                   </div>
                 </Col>
                 <Col className={styles.pricing_plan}>
-                  <div className={styles.plan_bg}><Image width="350" height="500" src="/Plans/green.svg" alt="plan background" /></div>
+                  <div className={styles.plan_bg}><Image width="400" height="600" src="/Plans/purple.svg" alt="plan background" /></div>
+                  <img src="https://wee.bio/assets/pattern/super-obj-1.png" alt="plan background" className={styles.obj_1} />
                   <div className={styles.super_tier}>
-                    <div className={styles.plan_title}>Super</div>
+                    <div className={styles.plan_title}>Combo</div>
                     <div className={styles.plan_price}>
-                      <div className={styles.dollar}>$</div>
-                      <div className={styles.price}>3</div>
-                      <div className={styles.type}>/ month</div>
+                      <div className={styles.dollar}>Rs.</div>
+                      <div className={styles.price}>250</div>
                     </div>
                     <div className={styles.plan_description}>
-                      <span>For Power Makers</span>
-                      <span> Growing their businesses and collecting a lot of feedback, advanced insights into their users sentiments.</span>
+                      <span>All Tech + All Non-Tech + Devathlon (Hackathon)</span>
                     </div>
                   </div>
                   <div className={styles.plan_action}>
-                    <a href="/register?plan=super" className={styles.btn + " " + styles.btn_big + " " + styles.btn_primary}>GET SUPER FOR $3</a>
+                    <a >Pay Now </a>
+                  </div>
+                  <div className={styles.plan_action}>
+                    <a >Upload Receipt</a>
+                  </div>
+                </Col>
+                <Col className={styles.pricing_plan}>
+                  <div className={styles.plan_bg}><Image width="400" height="600" src="/Plans/purple.svg" alt="plan background" /></div>
+                  <img src="https://wee.bio/assets/pattern/super-obj-1.png" alt="plan background" className={styles.obj_1} />
+                  <div className={styles.super_tier}>
+                    <div className={styles.plan_title}>Combo</div>
+                    <div className={styles.plan_price}>
+                      <div className={styles.dollar}>Rs.</div>
+                      <div className={styles.price}>200</div>
+                    </div>
+                    <div className={styles.plan_description}>
+                      <span>All Tech + All Non-Tech<br />&nbsp;</span>
+                    </div>
+                  </div>
+                  <div className={styles.plan_action}>
+                    <a >Pay Now </a>
+                  </div>
+                  <div className={styles.plan_action}>
+                    <a >Upload Receipt</a>
+                  </div>
+                </Col>
+                <Col className={styles.pricing_plan}>
+                  <div className={styles.plan_bg}><Image width="400" height="600" src="/Plans/green.svg" alt="plan background" /></div>
+                  <div className={styles.super_tier}>
+                    <div className={styles.plan_title}>Hackathon</div>
+                    <div className={styles.plan_price}>
+                      <div className={styles.dollar}>Rs.</div>
+                      <div className={styles.price}>100</div>
+                    </div>
+                    <div className={styles.plan_description}>
+                      <span>Devathlon<br />&nbsp; </span>
+                    </div>
+                  </div>
+                  <div className={styles.plan_action}>
+                    <a >Pay Now </a>
+                  </div>
+                  <div className={styles.plan_action}>
+                    <a >Upload Receipt</a>
+                  </div>
+                </Col>
+                <Col className={styles.pricing_plan}>
+                  <div className={styles.plan_bg}><Image width="400" height="600" src="/Plans/green.svg" alt="plan background" /></div>
+                  <div className={styles.super_tier}>
+                    <div className={styles.plan_title}>Hackathon</div>
+                    <div className={styles.plan_price}>
+                      <div className={styles.dollar}>Rs.</div>
+                      <div className={styles.price}>200</div>
+                    </div>
+                    <div className={styles.plan_description}>
+                      <span>Hackinfinity<br />&nbsp;</span>
+                    </div>
+                  </div>
+                  <div className={styles.plan_action}>
+                    <a >Pay Now </a>
+                  </div>
+                  <div className={styles.plan_action}>
+                    <a >Upload Receipt</a>
+                  </div>
+                </Col><Col className={styles.pricing_plan}>
+                  <div className={styles.plan_bg}><Image width="400" height="600" src="/Plans/green.svg" alt="plan background" /></div>
+                  <div className={styles.super_tier}>
+                    <div className={styles.plan_title}>Hackathon</div>
+                    <div className={styles.plan_price}>
+                      <div className={styles.dollar}>Rs.</div>
+                      <div className={styles.price}>100</div>
+                    </div>
+                    <div className={styles.plan_description}>
+                      <span>Biomedicathon<br />&nbsp;</span>
+                    </div>
+                  </div>
+                  <div className={styles.plan_action}>
+                    <a >Pay Now </a>
+                  </div>
+                  <div className={styles.plan_action}>
+                    <a >Upload Receipt</a>
+                  </div>
+                </Col>
+                <Col className={styles.pricing_plan}>
+                  <div className={styles.plan_bg}><Image width="400" height="600" src="/Plans/blue.svg" alt="plan background" /></div>
+                  <div className={styles.super_tier}>
+                    <div className={styles.plan_title}>Workshop</div>
+                    <div className={styles.plan_price}>
+                      <div className={styles.dollar}>Rs.</div>
+                      <div className={styles.price}>150</div>
+                    </div>
+                    <div className={styles.plan_description}>
+                      <span>AI/ML Workshop<br />&nbsp; < /span>
+                    </div>
+                  </div>
+                  <div className={styles.plan_action}>
+                    <a >Pay Now </a>
+                  </div>
+                  <div className={styles.plan_action}>
+                    <a >Upload Receipt</a>
+                  </div>
+                </Col>
+                <Col className={styles.pricing_plan}>
+                  <div className={styles.plan_bg}><Image width="400" height="600" src="/Plans/blue.svg" alt="plan background" /></div>
+                  <div className={styles.super_tier}>
+                    <div className={styles.plan_title}>Workshop</div>
+                    <div className={styles.plan_price}>
+                      <div className={styles.dollar}>Rs.</div>
+                      <div className={styles.price}>100</div>
+                    </div>
+                    <div className={styles.plan_description}>
+                      <span>Civil Workshop<br />&nbsp;</span>
+                    </div>
+                  </div>
+                  <div className={styles.plan_action}>
+                    <a >Pay Now </a>
+                  </div>
+                  <div className={styles.plan_action}>
+                    <a >Upload Receipt</a>
+                  </div>
+                </Col>
+                <Col className={styles.pricing_plan}>
+                  <div className={styles.plan_bg}><Image width="400" height="600" src="/Plans/pink.svg" alt="plan background" /></div>
+                  <div className={styles.super_tier}>
+                    <div className={styles.plan_title}>Events</div>
+                    <div className={styles.plan_price}>
+                      <div className={styles.dollar}>Rs.</div>
+                      <div className={styles.price}>150</div>
+                    </div>
+                    <div className={styles.plan_description}>
+                      <span>All Tech Events<br />&nbsp;</span>
+                    </div>
+                  </div>
+                  <div className={styles.plan_action}>
+                    <a >Pay Now </a>
+                  </div>
+                  <div className={styles.plan_action}>
+                    <a >Upload Receipt</a>
+                  </div>
+                </Col>
+                <Col className={styles.pricing_plan}>
+                  <div className={styles.plan_bg}><Image width="400" height="600" src="/Plans/pink.svg" alt="plan background" /></div>
+                  <div className={styles.super_tier}>
+                    <div className={styles.plan_title}>Events</div>
+                    <div className={styles.plan_price}>
+                      <div className={styles.dollar}>Rs.</div>
+                      <div className={styles.price}>100</div>
+                    </div>
+                    <div className={styles.plan_description}>
+                      <span>All Non-Tech Events<br />&nbsp;</span>
+                    </div>
+                  </div>
+                  <div className={styles.plan_action}>
+                    <a >Pay Now </a>
+                  </div>
+                  <div className={styles.plan_action}>
+                    <a >Upload Receipt</a>
                   </div>
                 </Col>
               </Row>
