28c316d294755c13bf74ec091c6f0e97e1079bc8
Sruthi Bijoy <sruthibijoyr@gmail.com>
2021-09-06 20:49:56 +0530

    Fix timer

--- public/js/timer.js
+++ public/js/timer.js
@@ -1,7 +1,7 @@
-const year = new Date().getFullYear();
-const invente = new Date(2021, 9, 7).getTime();
+year = new Date().getFullYear();
+invente = new Date(2021, 9, 7).getTime();
 
-let timer = setInterval(function () {
+timer = setInterval(function () {
   const today = new Date().getTime();
   let diff;
   diff = invente - today;
@@ -11,8 +11,15 @@ let timer = setInterval(function () {
   let minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
   let seconds = Math.floor((diff % (1000 * 60)) / 1000);
 
-  document.getElementById('days_num').innerHTML = days;
-  document.getElementById('hours_num').innerHTML = hours;
-  document.getElementById('minutes_num').innerHTML = minutes;
-  document.getElementById('seconds_num').innerHTML = seconds;
+  if (document.getElementById('days_num'))
+    document.getElementById('days_num').innerHTML = days;
+  if (document.getElementById('hours_num'))
+    document.getElementById('hours_num').innerHTML = hours;
+  if (document.getElementById('minutes_num'))
+    document.getElementById('minutes_num').innerHTML = minutes;
+  if (document.getElementById('seconds_num'))
+    document.getElementById('seconds_num').innerHTML = seconds;
+  else {
+    clearInterval(timer);
+  }
 }, 1000);
