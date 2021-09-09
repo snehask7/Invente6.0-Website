03964b74280a075aaa34d8338c71a3c995ebc56f
Sneha Sriram Kannan <52703275+snehask7@users.noreply.github.com>
2021-09-09 10:30:37 +0530

    Update action.js

--- pages/action.js
+++ pages/action.js
@@ -57,7 +57,7 @@ function AuthAction() {
       </div>
       {/* <NavbarComp /> */}
       <main className={styles2.page_wrapper}>
-        <div className={!navbarToggle ? styles.hide : styles.signinCard}>
+        <div className={styles.signinCard}>
           {mode == 'verifyEmail' ? (
             <>
               <h4 className={styles.mailBoxText}>
