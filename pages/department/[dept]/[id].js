01d4a6596dad5eec22171fa67991545a91b2fb8a
Sruthi Bijoy <sruthibijoyr@gmail.com>
2021-09-15 19:13:07 +0530

    Add prize money

--- pages/department/[dept]/[id].js
+++ pages/department/[dept]/[id].js
@@ -167,6 +167,47 @@ export default function Department({ data }) {
                         : 'Non-Tech'}
                     </span>
                   </p>
+                  <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
+                    <defs>
+                      <filter id="gooey">
+                        <feGaussianBlur
+                          in="SourceGraphic"
+                          stdDeviation="5"
+                          result="blur"
+                        />
+                        <feColorMatrix
+                          in="blur"
+                          type="matrix"
+                          values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9"
+                          result="highContrastGraphic"
+                        />
+                        <feComposite
+                          in="SourceGraphic"
+                          in2="highContrastGraphic"
+                          operator="atop"
+                        />
+                      </filter>
+                    </defs>
+                  </svg>
+
+                  <div className={styles.gooey_button}>
+                    <span className={styles.prizes_text}>
+                      Prizes Worth Rs. 10,000
+                    </span>
+
+                    <span className={styles.bubbles}>
+                      <span className={styles.bubble}></span>
+                      <span className={styles.bubble}></span>
+                      <span className={styles.bubble}></span>
+                      <span className={styles.bubble}></span>
+                      <span className={styles.bubble}></span>
+                      <span className={styles.bubble}></span>
+                      <span className={styles.bubble}></span>
+                      <span className={styles.bubble}></span>
+                      <span className={styles.bubble}></span>
+                      <span className={styles.bubble}></span>
+                    </span>
+                  </div>
                   <br></br>
                   {events[id].description.summary ? (
                     <>
